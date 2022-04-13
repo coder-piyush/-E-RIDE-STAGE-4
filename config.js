dc1c69dcdf1e1f750a73dcaa8aa19a78ac911d85
coder-piyush <85088718+coder-piyush@users.noreply.github.com>
2022-04-13 19:14:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 //Paste your firebaseConfig here
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBUunj96WwfAJ9tI9-HDaSMgdaCvLkvGFs",
+  authDomain: "e-ride-91580.firebaseapp.com",
+  projectId: "e-ride-91580",
+  storageBucket: "e-ride-91580.appspot.com",
+  messagingSenderId: "741921088103",
+  appId: "1:741921088103:web:6981f34a8a36798bcdeac2"
+};
 
 
 
